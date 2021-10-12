eb8c3f00268de7f7a4b5e0c0a34d27b0057949c9
Tuan Hung <tuanhungg26@gmail.com>
2021-10-12 20:40:07 +0700

    Add matran.cpp file

---
+++ matran.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+void nhap(int a[][100], int m, int n)
+  {
+  	int i,j;
+  	for (i=0;i<m;i++)
+  	for (j=0;j<n;j++)
+  	  {
+  	  	printf("a[%d][%d]=",i,j);
+  	  	scanf("%d",&a[i][j]);
+		}
+  }
+void xuat(int a[][100], int m, int n)
+  {
+  	int i,j;
+  	printf("\nMa tran A la:");
+	for (i=0;i<m;i++)
+  	  {
+  	    printf("\n");
+  	  	for (j=0;j<n;j++) printf("%5d",a[i][j]);
+		}
+  }
+int main()
+  {
+  	int a[100][100],m,n;
+  	printf("\nNhap so hang va so cot:");
+  	scanf("%d%d",&m,&n);
+  	nhap(a,m,n);
+  	xuat(a,m,n);
+  	getch();
+  	return 0;
+  }
